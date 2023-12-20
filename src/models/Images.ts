a0f723d724fcfa127b91a483418fcbc3fb908028
Muhammad Umer <mhadisb1212@gmail.com>
2023-12-20 00:01:11 -0800

    Streak

--- src/models/Images.ts
+++ src/models/Images.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 const BasicImageSchema = z.object({
   page: z.number(),
-  per_page: z.number(),
+  per_page: z.number()
   prev_page: z.string().optional(),
   next_page: z.string().optional(),
   total_results: z.number(),
